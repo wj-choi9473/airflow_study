fc0c96b9f140299542458c84a277626e9fb5b7e0
wj-choi9473 <52997573+wj-choi9473@users.noreply.github.com>
2024-05-08 10:45:59 +0900

    fix SyntaxError

--- dags/dags_bash_operators.py
+++ dags/dags_bash_operators.py
@@ -7,7 +7,7 @@ with DAG(
     dag_id="dags_bash_operator",
     schedule="0 0 * * *",
     start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
-    catchup=False
+    catchup=False,
     tags = ["example","study"]
 ) as dag:
     bash_task1 = BashOperator(

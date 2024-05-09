6124ed089a3d259bf342efb46c70506f039b5146
wj-choi9473 <52997573+wj-choi9473@users.noreply.github.com>
2024-05-09 09:48:25 +0900

    Create dags_python_task_decorator.py

---
+++ dags/dags_python_task_decorator.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import pendulum
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_task_decorator",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2024, 5, 9, tz="Asia/Seoul"),
+    catchup=False,
+    tags = ["example","study"]
+) as dag:
+    
+    @task(task_id="python_task_1")
+    def print_context(some_input):
+        print(some_input)
+
+    python_task_1 = print_context('task_decorator 실행')
+

61531ac65ace463613eb6b890f983e7411b661cc
wj-choi9473 <52997573+wj-choi9473@users.noreply.github.com>
2024-05-09 15:57:15 +0900

    Create dags_bash_with_macro_eg1.py

---
+++ dags/dags_bash_with_macro_eg1.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg1",
+    schedule="10 0 L * *",
+    start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    # START_DATE: 전월 말일,utc -> timezone변경, END_DATE: 1일 전
+    bash_task_1 = BashOperator(
+        task_id='bash_task_1',
+        env={'START_DATE':'{{ data_interval_start.in_timezone("Asia/Seoul") | ds }}', # | ds YYYY-MM-DD형식
+             #macros 이요하여 dateutil사용
+             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds}}'
+        },
+        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
+    )

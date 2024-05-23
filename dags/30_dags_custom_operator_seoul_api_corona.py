c547a00e545476055f6ad34835b84130b5455409
wj-choi9473 <52997573+wj-choi9473@users.noreply.github.com>
2024-05-23 09:53:18 +0900

    Create 30_dags_custom_operator_seoul_api_corona.py

---
+++ dags/30_dags_custom_operator_seoul_api_corona.py
@@ -0,0 +1,33 @@
+"""
+Custom Operator 활용 예시 DAG
+
+plugins/operators/seould_api_to_csv_operator.py
+data저장할 디렉토리 연결 필요 
+"""
+from operators.seoul_api_to_csv_operator import SeoulApiToCsvOperator
+from airflow import DAG
+import pendulum
+
+with DAG(
+    dag_id='dags_seoul_api_corona',
+    schedule='0 7 * * *',
+    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    '''서울시 코로나19 확진자 발생동향'''
+    tb_corona19_count_status = SeoulApiToCsvOperator(
+        task_id='tb_corona19_count_status',
+        dataset_nm='TbCorona19CountStatus', #open api명세 확인
+        path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
+        file_name='TbCorona19CountStatus.csv'
+    )
+    
+    '''서울시 코로나19 백신 예방접종 현황'''
+    tv_corona19_vaccine_stat_new = SeoulApiToCsvOperator(
+        task_id='tv_corona19_vaccine_stat_new',
+        dataset_nm='tvCorona19VaccinestatNew',
+        path='/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
+        file_name='tvCorona19VaccinestatNew.csv'
+    )
+
+    tb_corona19_count_status >> tv_corona19_vaccine_stat_new

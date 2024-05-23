295b966a3aa86624f631e1d32163b178b7006675
wj-choi9473 <52997573+wj-choi9473@users.noreply.github.com>
2024-05-23 09:53:42 +0900

    Create seoul_api_to_csv_operator.py

---
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -0,0 +1,71 @@
+"""
+Custom Operator
+장점 
+- 원하는대로 로직
+- 서울 공공데이터 데이터셋 추출/저장 파이썬 파일을 여러번 만들어 PythonOperator을 만들면 이 자체로 비효율 
+    -> 특정 기능 공통 모듈, 상세 조건은 파라미터로 만들어 모듈 재사용 유도
+
+서울시 open api 예제
+
+url:8080/apikey/type/dataset_nm/start/end
+"""
+
+from airflow.models.baseoperator import BaseOperator
+from airflow.hooks.base import BaseHook
+import pandas as pd 
+
+class SeoulApiToCsvOperator(BaseOperator):
+    template_fields = ('endpoint', 'path','file_name','base_dt')
+
+    def __init__(self, dataset_nm, path, file_name, base_dt=None, **kwargs):
+        super().__init__(**kwargs)
+        self.http_conn_id = 'openapi.seoul.go.kr'
+        self.path = path
+        self.file_name = file_name
+        self.endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json/' + dataset_nm
+        self.base_dt = base_dt
+
+    def execute(self, context):
+        import os
+        
+        connection = BaseHook.get_connection(self.http_conn_id) # gui에서 만든 connection정보
+        self.base_url = f'http://{connection.host}:{connection.port}/{self.endpoint}'
+
+        total_row_df = pd.DataFrame()
+        start_row = 1
+        end_row = 1000
+        while True:
+            self.log.info(f'시작:{start_row}') # log출력
+            self.log.info(f'끝:{end_row}')
+            row_df = self._call_api(self.base_url, start_row, end_row)
+            total_row_df = pd.concat([total_row_df, row_df])
+            if len(row_df) < 1000:
+                break
+            else:
+                start_row = end_row + 1
+                end_row += 1000
+
+        if not os.path.exists(self.path):
+            os.system(f'mkdir -p {self.path}')
+        total_row_df.to_csv(self.path + '/' + self.file_name, encoding='utf-8', index=False)
+
+    def _call_api(self, base_url, start_row, end_row):
+        import requests
+        import json 
+
+        headers = {'Content-Type': 'application/json',
+                   'charset': 'utf-8',
+                   'Accept': '*/*'
+                   }
+
+        request_url = f'{base_url}/{start_row}/{end_row}/'
+        if self.base_dt is not None:
+            request_url = f'{base_url}/{start_row}/{end_row}/{self.base_dt}'
+        response = requests.get(request_url, headers)
+        contents = json.loads(response.text)
+
+        key_nm = list(contents.keys())[0]
+        row_data = contents.get(key_nm).get('row')
+        row_df = pd.DataFrame(row_data)
+
+        return row_df
